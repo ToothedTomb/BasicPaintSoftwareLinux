e6597f09639f6a05530a65247d5cdab9667cf6f2
Jonathan Linux <52569279+ToothedTomb@users.noreply.github.com>
2022-06-11 15:21:43 +0100

    Add files via upload

--- paint.py
+++ paint.py
@@ -2,20 +2,77 @@ from cProfile import label
 from tkinter import *
 from tkinter.colorchooser import askcolor
 import tkinter.messagebox
+from tkinter import filedialog
+import tkinter.filedialog
+from typing_extensions import Self
+import tkinter as ttk 
+import sys
+from tkinter import Toplevel
+def on_closing():
+    root = tkinter.Toplevel()  
+    root.resizable(0,0)
+    root.title("Confirm to exit the software:")
+    root.tk.call('wm', 'iconphoto', root._w, tkinter.PhotoImage(file='paint.png'))
 
 
+    labelTitle = ttk.Label(root,font=("Ubuntu", 26,"bold","underline"),anchor='center', text="Confirm to exit the software:")
+    label = ttk.Label(root,font=("Ubuntu", 16,"bold",),anchor='center', text="Are you sure you want to leave this software?")
+
+    labelTitle.pack(side="top",fill="x",pady=1)
+    label.pack(side="top", fill="x", pady=2)
+    B1 = tkinter.Button(root, text="Yes",font=("ubuntu",28),bg="#3b8ee3",border=(1),activebackground="#20b58b", command = root.quit)
+
+    B2 = tkinter.Button(root, text="No",font=("ubuntu",28),bg="#3b8ee3",border=(1),activebackground="#20b58b", command = root.destroy)
+    B1.pack(side=tkinter.LEFT, anchor=CENTER)
+    B2.pack(side=tkinter.RIGHT, anchor=CENTER)
+def MadeBy():
+    root = tkinter.Toplevel()  
+    root.resizable(0,0)
+    root.title("Who made this software?")
+    root.tk.call('wm', 'iconphoto', root._w, tkinter.PhotoImage(file='paint.png'))
+
+
+    labelTitle = ttk.Label(root,font=("Ubuntu", 26,"bold","underline"),anchor='center', text="Who made this software?")
+    label = ttk.Label(root,font=("Ubuntu", 16,"bold",),anchor='center', text="Jonathan Steadman has made this software.")
+    labelTitle.pack(side="top",fill="x",pady=1)
+    label.pack(side="top", fill="x", pady=2)
+    B1 = tkinter.Button(root, text="Exit",font=("ubuntu",28),bg="#3b8ee3",border=(1),activebackground="#20b58b", command = root.destroy)
+    B1.pack()
+def WhatIsThis():
+    root = tkinter.Toplevel()  
+    root.resizable(0,0)
+    root.title("What is this software?")
+    root.tk.call('wm', 'iconphoto', root._w, tkinter.PhotoImage(file='paint.png'))
+
+
+    labelTitle = ttk.Label(root,font=("Ubuntu", 26,"bold","underline"),anchor='center', text="What is this software?")
+    label = ttk.Label(root,font=("Ubuntu", 16,"bold",),anchor='center', text="This software is a free and open source basic paint software for FreeBSD and Linux.")
+
+    labelTitle.pack(side="top",fill="x",pady=1)
+    label.pack(side="top", fill="x", pady=2)
+
+
+    B1 = tkinter.Button(root, text="Exit",font=("ubuntu",28),bg="#3b8ee3",border=(1),activebackground="#20b58b", command = root.destroy)
+    B1.pack()    
 class Paint(object):
 
     DEFAULT_PEN_SIZE = 100.0
     DEFAULT_COLOR = 'purple'
 
+
+
     def __init__(self):
         self.root = Tk()
         self.root.resizable(0,0)
         self.root.configure(bg='pink')
-        self.root.title("Basic Paint Software 1.0. (BETA)") 
+        self.root.title("Basic Paint Software 2.0. (BETA)") 
         self.root.tk.call('wm', 'iconphoto', self.root._w, tkinter.PhotoImage(file='paint.png'))
-
+        my_menu= Menu(self.root)
+        self.root.config(menu=my_menu)
+        file_menu= Menu(my_menu,background="pink",activebackground="#20b58b")
+        my_menu.add_cascade(label="About:",font=("Ubuntu",18),activebackground="#20b58b", menu=file_menu)
+        file_menu.add_command(label="Who made this Software?",font=("Ubuntu",18),activebackground="#20b58b",background="pink",command=MadeBy) 
+        file_menu.add_command(label="What is this software?",font=("Ubuntu",18),activebackground="#20b58b",background="pink",command=WhatIsThis) 
         self.color_button = Button(self.root, text='Change the color', font=("Ubuntu", 23, "bold"),bg="#3b8ee3",border=(1),activebackground="#20b58b", command=self.choose_color)
         self.color_button.grid(row=0, column=0)
         self.label = Label(self.root, text='Change The Size:',bg="pink",font=("ubuntu",23))
@@ -24,13 +81,14 @@ class Paint(object):
         self.choose_size_button.grid(row=0, column=2)
         self.color_button = Button(self.root, text='Save image', font=("Ubuntu", 23, "bold"),bg="#3b8ee3",border=(1),activebackground="#20b58b", command=self.saveimage)
         self.color_button.grid(row=0, column=3)
-
-        self.c = Canvas(self.root, bg='white', width=1024, height=700)
+        self.c = Canvas(self.root, bg='white', width=1224, height=780)
         self.c.grid(row=1, columnspan=5)
+        self.root.protocol("WM_DELETE_WINDOW", on_closing)
 
         self.setup()
         self.root.mainloop()
 
+        
     def setup(self):
         self.old_x = None
         self.old_y = None
@@ -41,10 +99,11 @@ class Paint(object):
         self.c.bind('<ButtonRelease-1>', self.reset)
 
     def saveimage(self):
-        tkinter.messagebox.showinfo("Coming out soon.",  "This is still being worked on but when done. I will update this software. For now you can just take a screenshot. ;)")
+        
+        self.c.update()
+        self.c.postscript(file="Drawing.ps", colormode='color')
 
-
-
+ 
     def choose_color(self):
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
@@ -69,6 +128,9 @@ class Paint(object):
 
     def reset(self, event):
         self.old_x, self.old_y = None, None
+
+
+
 
 
 if __name__ == '__main__':
